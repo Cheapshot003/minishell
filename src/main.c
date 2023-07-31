187366649b10f1e308d9c07e1335807b49985bc1
Ole Tietz <otietz@c4r1p10.42vienna.com>
2023-07-31 10:10:36 +0200

    fixing valgrind errors

--- src/main.c
+++ src/main.c
@@ -10,16 +10,16 @@ int main()
   t_data data;
   char *line;
   char **tokens;
-  //char *prompt;
+  char *prompt;
   signal(SIGINT, intHandler);
   fill_path(&data);
   using_history();
   while (1)
   {
     data.working_dir = getcwd(NULL, 0);
-    //prompt = getPrompt(data.working_dir);
-    line = readline("Test");
-    //free(prompt);
+    prompt = getPrompt(data.working_dir);
+	line = readline(prompt);
+    free(prompt);
     if (line[0] == '\0')
       continue;
     if (line && *line)
@@ -36,7 +36,7 @@ char *getPrompt(char *working_dir)
 {
   char *prompt;
 
-  prompt = malloc(strlen(working_dir + 3));
+  prompt = malloc(strlen(working_dir) + 3);
   prompt[0] = '\0';
   strcat(prompt, working_dir);
   strcat(prompt, "$ ");
